c9214d88955289e29b3287f3c6556218044ce973
Dima YaFlay <93622229+YaFlay@users.noreply.github.com>
2021-11-03 01:21:42 +0300

    Create index.js
    create js files

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+function sayHi(){
+    alert('Hello World!');
+}
+sayHi()
+let a = confirm('Do u like my work')
+if (a == true){
+    alert('Thanks!!')
+}else{
+    alert('Ok, sorry')
+}
